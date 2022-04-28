b7f95301d38ec539a07c70e97ffeceff939c511b
Guillaume Larroque <larroque.guillaume@gmail.com>
2022-04-28 12:10:51 +0200

    Ajout de json-server + data dans db.son

--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -1,41 +1,48 @@
 <template>
-    <h1>Jobs</h1>
+  <h1>Jobs</h1>
+  <div v-if="jobs.length">
     <div v-for="job in jobs" :key="job.id" class="job">
-        <router-link :to='{ name: "JobDetails", params: {id: job.id} }'>
-            <h2>{{ job.title }}</h2>
-        </router-link>
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
     </div>
+  </div>
+  <div v-else>
+    Loading Jobs...
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'JobsView',
-    data(){
-        return {
-            jobs: [
-                { title: 'Ninja UX designer', id: 1, details: 'lorem'},
-                { title: 'Ninja web developper', id: 2, details: 'lorem'},
-                { title: 'Ninja vue developper', id: 3, details: 'lorem'},
-            ]
-        }
-    }
-}
+  name: "JobsView",
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
-.job h2{
-    background: #F4F4F4;
-    padding: 20px;
-    border-radius: 10px;
-    margin: 10px auto;
-    max-width: 600px;
-    cursor: pointer;
-    color: #444;
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
 }
-.job h2:hover{
-    background: #ddd;
+.job h2:hover {
+  background: #ddd;
 }
-.job a{
-    text-decoration: none;
+.job a {
+  text-decoration: none;
 }
 </style>
